b4e195915375f44c62875641a55e0f9a800e220f
Bruno Alexandre <bruno.in.dk@gmail.com>
2011-08-20 00:56:24 +0200

    first commit

---
+++ mainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace StackOverflowQuestion7127946
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pRight = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pLeft = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pRight
+            // 
+            this.pRight.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pRight.Location = new System.Drawing.Point(309, 81);
+            this.pRight.Name = "pRight";
+            this.pRight.Size = new System.Drawing.Size(244, 286);
+            this.pRight.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Left panel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Right panel";
+            // 
+            // pLeft
+            // 
+            this.pLeft.BackColor = System.Drawing.SystemColors.Info;
+            this.pLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pLeft.Location = new System.Drawing.Point(26, 81);
+            this.pLeft.Name = "pLeft";
+            this.pLeft.Size = new System.Drawing.Size(244, 286);
+            this.pLeft.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 396);
+            this.Controls.Add(this.pLeft);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pRight);
+            this.Name = "mainForm";
+            this.Text = "StackOverflow Question 7127946";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pRight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pLeft;
+    }
+}
+
